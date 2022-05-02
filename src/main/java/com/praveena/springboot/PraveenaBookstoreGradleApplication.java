da6f7d03f77022cf57d9b7a744f4d02eb5f8a5b6
prava <praveena@gmail.com>
2022-05-02 16:38:57 +0530

    first commit of praveenabookstore

---
+++ src/main/java/com/praveena/springboot/PraveenaBookstoreGradleApplication.java
@@ -0,0 +1,13 @@
+package com.praveena.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraveenaBookstoreGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraveenaBookstoreGradleApplication.class, args);
+	}
+
+}
